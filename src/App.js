0b45a5de751717b1de7c2fc411eff10d646ddda9
Gbemisola Odukoya <folaryz@gmail.com>
2022-09-03 15:18:13 +0100

    Created Count Component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./App.css";
 
 export default function App() {
   /**
@@ -8,11 +9,11 @@ export default function App() {
   const [count, setCount] = React.useState(0);
 
   function add() {
-    setCount(count + 1);
+    setCount((prevCount) => prevCount + 1);
   }
 
   function minus() {
-    setCount(count - 1);
+    setCount((prevCount) => prevCount - 1);
   }
 
   return (
@@ -29,3 +30,17 @@ export default function App() {
     </div>
   );
 }
+
+/**
+ * Note: if you ever need the old value of state
+ * to help you determine the new value of state,
+ * you should pass a callback function to your
+ * state setter function instead of using
+ * state directly. This callback function will
+ * receive the old value of state as its parameter,
+ * which you can then use to determine your new
+ * value of state.
+ */
+//    function add() {
+//     setCount(prevCount => prevCount + 1)
+// }
---
+++ src/Count.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Count = (number) => {
+  return <div></div>;
+};
+
+export default Count;
